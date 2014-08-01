8633166234677da6ab0eda6895e3047cd9c6a2c9
senthilraj <senthil2rajan@gmail.com>
2014-08-01 19:55:28 +0530

    first update

---
+++ js/index.js
@@ -0,0 +1,79 @@
+
+
+
+$( ".open" ).click(function() {
+  //alert( "Handler for .click() called." );
+          $(this).fadeOut();
+          //$(".shutter").css({"top":"-865px"});
+          $( ".shutter" ).animate({top: "-590px"}, 1500);
+          setTimeout(function (){
+              $("#section1 h1").fadeIn(1500);
+            },1700);
+            setTimeout(function (){
+                $("#nav").fadeIn(1000);
+            },4000);
+});
+
+ 
+ 
+$( "#sec2btn" ).click(function() { 
+    //alert("sdf");
+    setTimeout(function (){
+        $("#section2 h1").fadeIn(1000);
+        $("ul.skills li").fadeIn(1000);
+                                        $('.loader1').ClassyLoader({
+                                                speed: 20,
+                                                diameter: 60,
+                                                fontSize: '30px',
+                                                fontFamily: 'Courier',
+                                                fontColor: '#fff',
+                                                lineColor: '#fff',
+                                                percentage: 80,
+                                                lineWidth: 15,
+                                                width: 150, // width of the loader in pixels
+                                                height: 150,
+                                                remainingLineColor: 'rgba(200,200,200,0.1)'
+                                            });
+                                            $('.loader2').ClassyLoader({
+                                                speed: 20,
+                                                diameter: 60,
+                                                fontSize: '30px',
+                                                fontFamily: 'Courier',
+                                                fontColor: '#fff',
+                                                lineColor: '#fff',
+                                                percentage: 74,
+                                                lineWidth: 15,
+                                                width: 150, // width of the loader in pixels
+                                                height: 150,
+                                                remainingLineColor: 'rgba(200,200,200,0.1)'
+                                            });
+                                            $('.loader3').ClassyLoader({
+                                                speed: 20,
+                                                diameter: 60,
+                                                fontSize: '30px',
+                                                fontFamily: 'Courier',
+                                                fontColor: '#fff',
+                                                lineColor: '#fff',
+                                                percentage: 78,
+                                                lineWidth: 15,
+                                                width: 150, // width of the loader in pixels
+                                                height: 150,
+                                                remainingLineColor: 'rgba(200,200,200,0.1)'
+                                            });
+                                            $('.loader4').ClassyLoader({
+                                                speed: 20,
+                                                diameter: 60,
+                                                fontSize: '30px',
+                                                fontFamily: 'Courier',
+                                                fontColor: '#fff',
+                                                lineColor: '#fff',
+                                                percentage: 70,
+                                                lineWidth: 15,
+                                                width: 150, // width of the loader in pixels
+                                                height: 150,
+                                                remainingLineColor: 'rgba(200,200,200,0.1)'
+                                            });
+            },1000);
+                    
+});
+$("#nav").scroll_navi();
