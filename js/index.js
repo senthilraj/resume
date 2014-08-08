9bb431f1d55315f414dfaa69b79a3596c3f0a37c
senthil rajan <senthil2rajan@gmail.com>
2014-08-08 16:24:57 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -127,7 +127,7 @@ $( "#sec2btn" ).click(function() {
 
 $("#sec3btn").click(function(){
 
-  $( ".car" ).animate({right: "1500px"}, 8000, function () {
+  $( ".car" ).animate({right: "1500px"}, 7000, function () {
       
       $( ".car" ).css({right: "-600px"});
       //$( ".car" ).css({transform: translate(0, 0)});
