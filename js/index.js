660be5f7e037c49490b0cdc52a26a3091db7d055
senthil rajan <senthil2rajan@gmail.com>
2014-08-08 15:03:21 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -119,13 +119,13 @@ $( "#sec2btn" ).click(function() {
                     
 });
 
-$("#sec3btn").click(function(){
+/*$("#sec3btn").click(function(){
   
   $( ".car" ).animate({right: "1500px"}, 8000, function () {
       
       $( ".car" ).css({right: "-600px"});
     
-  });
+  });*/
   
 });
 $("#nav").scroll_navi();
